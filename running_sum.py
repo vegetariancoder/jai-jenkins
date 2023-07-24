c91fb94fa6b556e42b10bbcfa0aa1b65734df4d3
sahilnagpal <thisissahilnagpal@gmail.com>
2023-07-24 13:53:11 -0400

    jenkins code

--- running_sum.py
+++ running_sum.py
@@ -1,8 +1,16 @@
+"""
+This module provides a function to calculate the running sum of a list of numbers.
+"""
 def running_sum(nums):
+    """
+    Calculate the running sum of a list of numbers.
+
+    :param nums: list of nums
+    :return: integer
+    """
     if isinstance(nums,list):
         run_sum = sum(nums)
         return run_sum
-    else:
-        return "Please enter the list for input"
+    return "Please enter the list for input"
 
 print(running_sum([1,2,3,4]))
