06f04afb7b8314000eb635a6dec9d5c0285d5886
sahilnagpal <thisissahilnagpal@gmail.com>
2023-07-24 15:09:20 -0400

    jenkins code

--- test_operations.py
+++ test_operations.py
@@ -1,5 +1,5 @@
 """
-This module will test running sum
+This module is to test running sum
 """
 import unittest
 from running_sum import running_sum
@@ -10,14 +10,27 @@ class TestMain(unittest.TestCase):
     This is test running_sum
     """
 
-    def __init__(self, nums):
-        super().__init__()
-        self.nums = nums
-
-    def test_running_sum(self):
+    def test_running_sum_with_valid_input(self):
         """
-
-        :return: assertion
+        Test running_sum with a valid input list.
         """
-        self.nums = [1,2,3,4]
-        self.assertEqual(running_sum(self.nums),10)
+        nums = [1, 2, 3, 4]
+        self.assertEqual(running_sum(nums), 10)
+
+    def test_running_sum_with_empty_list(self):
+        """
+        Test running_sum with an empty input list.
+        """
+        nums = []
+        self.assertEqual(running_sum(nums), 0)
+
+    def test_running_sum_with_non_list_input(self):
+        """
+        Test running_sum with a non-list input.
+        """
+        nums = 10
+        self.assertEqual(running_sum(nums), "Please enter the list for input")
+
+
+if __name__ == '__main__':
+    unittest.main()
